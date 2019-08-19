acda118aa97f25adf1b4071cce4298781b3e6a9c
snow <717688908@qq.com>
2019-08-19 20:39:31 +0800

    update

--- app-sales-api/src/main/java/com/xjy/app/sales/cashier/controller/HouseController.java
+++ app-sales-api/src/main/java/com/xjy/app/sales/cashier/controller/HouseController.java
@@ -22,7 +22,7 @@ public class HouseController {
     /**
      * 查询单条House信息
      * @param houseid 主键
-     * @return 结果集16890
+     * @return 结果集168901
      */
     @RequestMapping("/query/{houseid}")
     public Result queryByHouseId(@PathVariable("houseid") Integer houseid) {
